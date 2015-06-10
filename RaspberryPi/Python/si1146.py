8398f6eb0220012a6d057261cef5f350b6d26d7f
root <root@raspberrypi.(none)>
2015-06-10 12:57:59 +0000

    code update

--- RaspberryPi/Python/si1146.py
+++ RaspberryPi/Python/si1146.py
@@ -37,6 +37,10 @@ CHIP_STAT = 	0x30
 ANA_IN_KEY =  	0x3B
 
 #Commands for entering into command Resegister
+COM_PARAM_QUERY = (0b100 << 5)
+COM_PARAM_SET =  (0b101 << 5)
+COM_NOP = 		0b00000000
+COM_RESET = 		0b00000001
 
 COM_ALS_FORCE = 	0b00000110 #Forces a single ALS measurement
 COM_PSAL_FORCE =	0b00000111 #Forces a single PS and ALS measurement
@@ -53,11 +57,13 @@ COM_HW_KEY = 0x17 #Key to set Hardware from Sleep to Active mode.
 
 SI1146 = 	0b01000110
 
+
+
 class chipStat():
   def __init__(self):
     self.running = 0 
-	self.suspend = 0
-	self.sleep = 0
+    self.suspend = 0
+    self.sleep = 0
     
   
 
@@ -66,16 +72,25 @@ chip = chipStat()
 import smbus
 from  time import sleep as sleep
 
+bus = smbus.SMBus(1)
 
-def init(bus):
-  bus = smbus.SMBus(bus)
+def init():
   if (bus.read_byte_data(SI1146ADDR, PART_ID) == SI1146):
-    print((' SI1146 Detected on I2C BUS ' + char(bus)))
+    print((' SI1146 Detected on I2C BUS ' + str(bus)))
   elif(): 
-    print(('SI1146 Not found on I2C BUS  '+ char(bus)))
+    print(('SI1146 Not found on I2C BUS  '+ str(bus)))
     exit()
 
+def combineValues(MSB,LSB):
+  value = (MSB << 7 ) + LSB
+  return value
 
+def setReg(reg , data):
+  bus.write_block_data(SI1146ADDR, reg, [data])
+
+def getReg(reg):
+  data = bus.read_byte_data(SI1146ADDR,reg)
+  return data  
 
 #TODO Check status 
 
@@ -84,21 +99,71 @@ def getStatus():
   SUSPEND = 0b00000010
   SLEEP   = 0b00000001
   status = bus.read_byte_data(SI1146ADDR, CHIP_STAT)
+  chip.running =  (status & RUNNING)
   return ( (status & RUNNING) , (status & SUSPEND) ,(status&SLEEP))
-  
-  
+
+
+def sendReset(delay):
+  print('Resetting SI114X........')
+  bus.write_byte_data(SI1146ADDR, COMMAND, COM_RESET)
+  sleep(delay)
+
+def sendCommand(command):
+  while (getResponse() > 0):
+    bus.write_byte_data(SI1146ADDR, COMMAND, 0x00)
+    sleep(0.1)
+  while (getResponse() == 0):
+    bus.write_byte_data(SI1146ADDR, COMMAND, command)
+  print('OK')
+  return  
+
+def setParam(param,data):
+  setReg(PARAM_WR,data)
+  bus.write_byte_data(SI1146ADDR, COMMAND,(COM_PARAM_SET+param))
+  if (getReg(PARAM_RD)==data):
+    print('PARAM OK')
+  return
+
+def getResponse():
+  response = bus.read_byte_data(SI1146ADDR,RESPONSE)
+  return response
+
+def setRunning():
+  bus.write_byte_data(SI1146ADDR, HW_KEY , COM_HW_KEY)
+  return
+
+def readSensor(reg ):
+  lsb = getReg(PS1_DATA0)
+  msb = getReg(PS1_DATA1)
+  data = combineValues(msb,lsb)
+  return data
+
+def dumpRam(): 
+  for location in range(10):
+    sendCommand(COM_PARAM_QUERY+location)
+    data = getReg(PARAM_RD)
+    print (location ,' = ' ,data)
 
 
 
 
 
+init()
+sendReset(0.5)
+print(getStatus())
 
+sendCommand(COM_PS_AUTO)
+print(getResponse(), ' Commands Sent')
+setParam(0x01, 0b00010000)
+setReg(MEAS_RATE0, 0x01)
+sendCommand(COM_PS_AUTO)
+setRunning()
+print(getStatus())
+print(chip.running)
 
+dumpRam()
 
-
-
-
-
-
-init(1)
+while 1: 
+  print (readSensor(0))
+  sleep(0.1)
 
